aea3f75e21e918514965c6126c527af18093e4fb
umbertofaenza <umberto.faenza@gmail.com>
2023-07-26 15:52:04 +0200

    fix: add = to if conditions

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const firstUserNumber = parseInt(prompt("Inserisci un numero"));
 const secondUserNumber = parseInt(prompt("Inserisci un altro numero"));
 let randomNumber;
 
-if (firstUserNumber < secondUserNumber) {
+if (firstUserNumber <= secondUserNumber) {
   randomNumber =
     Math.floor(Math.random() * (secondUserNumber - firstUserNumber)) +
     firstUserNumber;
-} else if (secondUserNumber < firstUserNumber) {
+} else if (secondUserNumber <= firstUserNumber) {
   randomNumber =
     Math.floor(Math.random() * (firstUserNumber - secondUserNumber)) +
     secondUserNumber;
