eaaab65facbd87bd0bc677b445460ef96c0cbceb
umbertofaenza <umberto.faenza@gmail.com>
2023-07-26 15:13:53 +0200

    feat: add traccia 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const firstUserNumber = parseInt(prompt("Inserisci un numero"));
+const secondUserNumber = parseInt(prompt("Inserisci un altro numero"));
+let randomNumber;
+
+if (firstUserNumber < secondUserNumber) {
+  randomNumber = Math.floor(Math.random() * secondUserNumber) + firstUserNumber;
+} else if (secondUserNumber < firstUserNumber) {
+  randomNumber = Math.floor(Math.random() * firstUserNumber) + secondUserNumber;
+}
+
+console.log(randomNumber);
