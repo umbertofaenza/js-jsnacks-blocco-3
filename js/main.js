177c20f6a649047ae9dd123a106b12091c46ed61
umbertofaenza <umberto.faenza@gmail.com>
2023-07-26 15:59:33 +0200

    feat: add traccia 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-const firstUserNumber = parseInt(prompt("Inserisci un numero"));
-const secondUserNumber = parseInt(prompt("Inserisci un altro numero"));
-let randomNumber;
+// # Traccia 1
 
-if (firstUserNumber <= secondUserNumber) {
-  randomNumber =
-    Math.floor(Math.random() * (secondUserNumber - firstUserNumber)) +
-    firstUserNumber;
-} else if (secondUserNumber <= firstUserNumber) {
-  randomNumber =
-    Math.floor(Math.random() * (firstUserNumber - secondUserNumber)) +
-    secondUserNumber;
+// const firstUserNumber = parseInt(prompt("Inserisci un numero"));
+// const secondUserNumber = parseInt(prompt("Inserisci un altro numero"));
+// let randomNumber;
+
+// if (firstUserNumber <= secondUserNumber) {
+//   randomNumber =
+//     Math.floor(Math.random() * (secondUserNumber - firstUserNumber)) +
+//     firstUserNumber;
+// } else if (secondUserNumber <= firstUserNumber) {
+//   randomNumber =
+//     Math.floor(Math.random() * (firstUserNumber - secondUserNumber)) +
+//     secondUserNumber;
+// }
+
+// console.log(randomNumber);
+
+// # Traccia 2
+
+const firstWord = prompt("Inserisci una parola");
+const secondWord = prompt("Inserisci un'altra parola");
+
+if (firstWord.length == secondWord.length) {
+  console.log(firstWord + " " + secondWord);
+} else if (firstWord.length > secondWord.length) {
+  console.log(firstWord);
+} else {
+  console.log(secondWord);
 }
-
-console.log(randomNumber);
