fbedd09493474c0fd9d5d09c7d38a46b08798b4c
umbertofaenza <umberto.faenza@gmail.com>
2023-07-26 15:47:17 +0200

    fix: subtract base number

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ const secondUserNumber = parseInt(prompt("Inserisci un altro numero"));
 let randomNumber;
 
 if (firstUserNumber < secondUserNumber) {
-  randomNumber = Math.floor(Math.random() * secondUserNumber) + firstUserNumber;
+  randomNumber =
+    Math.floor(Math.random() * (secondUserNumber - firstUserNumber)) +
+    firstUserNumber;
 } else if (secondUserNumber < firstUserNumber) {
-  randomNumber = Math.floor(Math.random() * firstUserNumber) + secondUserNumber;
+  randomNumber =
+    Math.floor(Math.random() * (firstUserNumber - secondUserNumber)) +
+    secondUserNumber;
 }
 
 console.log(randomNumber);
