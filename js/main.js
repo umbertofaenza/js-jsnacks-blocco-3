3c3dfbf05f06acb85d4c70b142dae4ae6bc8a1e2
umbertofaenza <umberto.faenza@gmail.com>
2023-07-26 17:22:10 +0200

    fix: evenNumbers array

--- js/main.js
+++ js/main.js
@@ -58,11 +58,11 @@
 // # Traccia 5
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let evenNumbers = "";
+let evenNumbers = [];
 
 for (let i = 0; i < numbers.length - 1; i++) {
   if (i % 2 != 0) {
-    evenNumbers += i;
+    evenNumbers.push(i);
   }
 }
 console.log(evenNumbers);
